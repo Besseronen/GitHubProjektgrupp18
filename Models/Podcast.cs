f597a93006e216b7e655691f7bfbac375c1da5ba
Besseronen <81746123+Besseronen@users.noreply.github.com>
2021-10-26 21:06:53 +0200

    Update

--- Models/Podcast.cs
+++ Models/Podcast.cs
@@ -9,7 +9,7 @@ namespace Nya_projektet_Grupp_18_C_Projekt.Models
        public class Podcast : Feed
         {
             public Podcast(string url, string uppdateringsIntervall, string kategori) :
-                base(kategori, uppdateringsIntervall)
+                base()
             {
                 
             }
