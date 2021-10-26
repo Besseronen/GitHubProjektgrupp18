012c11afa6d6e62cf7cf91a350309daaad9aa609
Besseronen <81746123+Besseronen@users.noreply.github.com>
2021-10-26 20:35:09 +0200

    Update

---
+++ Nya projektet Grupp 18 C-Projekt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nya_projektet_Grupp_18_C_Projekt
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+        }
+    }
+}
